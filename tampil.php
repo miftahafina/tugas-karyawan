f5f02263f8b687276a66f938d6b924e6fa88ed82
Miftah Afina <miftah.afina@gmail.com>
2016-09-22 23:28:16 +0700

    Menambahkan halaman tampil.php

---
+++ tampil.php
@@ -0,0 +1,50 @@
+<?php
+// memanggil koneksi
+include "koneksi.php";
+
+// query
+$query = "SELECT * FROM `karyawan` ORDER BY `nip` ASC";
+
+// jalankan query
+$eksekusi = mysqli_query($db, $query);
+
+// menampung semua data
+$data_karyawan = [];
+
+// set penomoran
+$nomor = 1;
+
+while ($row = mysqli_fetch_assoc($eksekusi)) {
+    $data_karyawan[] = $row;
+}
+?>
+
+<!-- menampilkan tabel -->
+<h1>Tabel Data Karyawan</h1>
+<table border="1">
+    <tr>
+        <th>No</th>
+        <th>NIP</th>
+        <th>Nama</th>
+        <th>Jabatan</th>
+        <th>Alamat</th>
+    </tr>
+
+    <?php if (empty($data_karyawan)) : ?>
+    <tr>
+        <td colspan="5">Tidak Ada data</td>
+    </tr>
+    <?php else : ?>
+
+    <?php foreach ($data_karyawan as $karyawan) : ?>
+    <tr>
+        <td><?php echo $nomor++ ?></td>
+        <td><?php echo $karyawan['nip'] ?></td>
+        <td><?php echo $karyawan['nama'] ?></td>
+        <td><?php echo $karyawan['jabatan'] ?></td>
+        <td><?php echo $karyawan['alamat'] ?></td>
+    </tr>
+    <?php endforeach ?>
+
+    <?php endif ?>
+</table>
